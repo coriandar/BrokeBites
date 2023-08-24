50c14f597cb9dcb4807aedc8a82981bbd8e45e72
jamielee <85136381+lleejamiee@users.noreply.github.com>
2023-08-24 16:10:53 +1200

    Add autentication.tsx, add export to auth in firebase.tsx

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -13,6 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
