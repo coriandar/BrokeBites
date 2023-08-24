aaf041f16276d7197f3c156140f88459b388ecc2
Jarcino <jvcorstorphine@gmail.com>
2023-08-24 17:48:55 +1200

    Add map marker and database implementation

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -1,17 +1,23 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.FB_API_KEY,
-    authDomain: process.env.FB_AUTH_DOMAIN,
-    projectId: process.env.FB_PROJECT_ID,
-    storageBucket: process.env.FB_STORAGE_BUCKET,
-    messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
-    appId: process.env.FB_APP_ID,
-    measurementId: process.env.FB_MEASUREMENT_ID,
+    apiKey: "AIzaSyBxCg3NYRGP49TAfURnknBeGHqBI9GKDT4",
+    authDomain: "brokebites-2023.firebaseapp.com",
+    projectId: "brokebites-2023",
+    storageBucket: "brokebites-2023.appspot.com",
+    messagingSenderId: "582483629923",
+    appId: "1:582483629923:web:59b3eaff14f8fab4eda556",
+    measurementId: "G-1YGDSLZJB9",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
