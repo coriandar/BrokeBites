cd6648198e3fdaea97da407bd5c1d9a5fb7e8d47
coriandar <88608040+coriandar@users.noreply.github.com>
2023-08-25 19:20:46 +1200

    Remove api keys

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import InitMap from "../components/Map";
 import { Auth } from "../components/Auth";
 
 const inter = Inter({ subsets: ["latin"] });
