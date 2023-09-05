9db1ac1043f2ccfcebd3226a8f1e8728218aeac5
jamielee <85136381+lleejamiee@users.noreply.github.com>
2023-09-05 20:01:26 +1200

    Add bone structure for email and password update

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -2,8 +2,15 @@ import { useState } from "react";
 import { auth, googleProvider } from "../config/firebase";
 import {
     createUserWithEmailAndPassword,
+    getAuth,
+    signInWithEmailAndPassword,
     signInWithPopup,
     signOut,
+    updateEmail,
+    updatePassword,
+    reauthenticateWithCredential,
+    verifyBeforeUpdateEmail,
+    AuthCredential,
 } from "firebase/auth";
 import { error } from "console";
 
@@ -12,6 +19,7 @@ export const Auth = () => {
     const [signInPassword, setSignInPassword] = useState("");
     const [signUpEmail, setSignUpEmail] = useState("");
     const [signUpPassword, setSignUpPassword] = useState("");
+
     const signUp = async () => {
         try {
             const user = await createUserWithEmailAndPassword(
@@ -26,11 +34,7 @@ export const Auth = () => {
 
     const signIn = async () => {
         try {
-            await createUserWithEmailAndPassword(
-                auth,
-                signInEmail,
-                signInPassword
-            );
+            await signInWithEmailAndPassword(auth, signInEmail, signInPassword);
         } catch (err) {
             console.error(err);
         }
@@ -47,10 +51,61 @@ export const Auth = () => {
     const logout = async () => {
         try {
             await signOut(auth);
+
+            // Page after sign out
         } catch (err) {
             console.error(err);
         }
     };
+
+    const updateEmailAddress = async () => {
+        try {
+            const auth = getAuth();
+            const user = auth.currentUser;
+
+            // Promp user to enter new email address. Possibily another function that returns string?
+            let newEmailAddress: string;
+
+            verifyBeforeUpdateEmail(user!, newEmailAddress!).then(() => {
+                // Confirmation after the email address update
+            });
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    const changePassword = async () => {
+        try {
+            const auth = getAuth();
+            const user = auth.currentUser;
+
+            const credential = promptForCredentials();
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    const reauthentication = async () => {
+        const auth = getAuth();
+        const user = auth.currentUser;
+
+        const credential = promptForCredentials();
+
+        reauthenticateWithCredential(user!, credential!)
+            .then(() => {
+                // User reauthenticated, redirect page
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    // TODO: prompt the user to re-provide their sign in credentials.
+    function promptForCredentials(): AuthCredential {
+        let credential!: AuthCredential;
+
+        return credential;
+    }
 
     return (
         <div>
