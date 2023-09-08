f03d1e937aa1f04a97c99e207b06ed448ca53479
Jamie Corstorphine <jvcorstorphine@gmail.com>
2023-09-08 14:15:57 +1200

    Refactored setRestaurantList into the dashboard class

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,19 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-import { getAllRestaurants } from "../config/firebase";
-import { getDocs, collection } from "firebase/firestore";
-import MarkerDetails from "./MarkerDetails";
 
 const libraries = ["places"];
 const mapApiKey = process.env.NEXT_PUBLIC_FB_API_KEY;
 
-// Set Map size
 const mapContainerStyle = {
     width: "80vw",
     height: "80vh",
 };
 
-//Set Map Styles (specifically, turn off points of interest)
 const mapStyles = [
     {
         featureType: "poi",
@@ -22,10 +17,11 @@ const mapStyles = [
     },
 ];
 
-//Function for rendering map
 const InitMap = ({
+    restaurantList,
     setRestaurantSelected,
 }: {
+    restaurantList: any[];
     setRestaurantSelected: Function;
 }) => {
     const { isLoaded, loadError } = useLoadScript({
@@ -33,33 +29,14 @@ const InitMap = ({
         libraries: libraries as any,
     });
 
-    //create setRestaurantList method
-    const [restaurantList, setRestaurantList] = useState<any>([]);
-
-    const [isMarkerClicked, setIsMarkerClicked] = useState(false);
-
     const handleMarkerClick = (restaurant: any) => {
         console.log(restaurant);
         setRestaurantSelected(restaurant);
     };
 
-    //create getRestaurantList method
-
-    // use getRestaurantList method when the map first renders
-    useEffect(() => {
-        const fetchData = async () => {
-            const restaurants = await getAllRestaurants();
-            setRestaurantList(restaurants);
-        };
-
-        fetchData();
-    }, []);
-
-    //displays loading or loading error for map
     if (loadError) return <div>Error loading maps</div>;
     if (!isLoaded) return <div>Loading...</div>;
 
-    //Returns GoogleMap centered on AUT with restaurants highlighted by a marker
     return (
         <div id="map">
             <GoogleMap
@@ -68,25 +45,17 @@ const InitMap = ({
                 mapContainerStyle={mapContainerStyle}
                 options={{ styles: mapStyles }}
             >
-                {restaurantList.map(
-                    (restaurant: {
-                        id: React.Key | null | undefined;
-                        latitude: any;
-                        longitude: any;
-                        name: string | undefined;
-                        website: string | undefined;
-                    }) => (
-                        <Marker
-                            key={restaurant.id}
-                            position={{
-                                lat: restaurant.latitude,
-                                lng: restaurant.longitude,
-                            }}
-                            title={restaurant.name} // Display the restaurant name on marker hover
-                            onClick={() => handleMarkerClick(restaurant)}
-                        />
-                    )
-                )}
+                {restaurantList.map((restaurant) => (
+                    <Marker
+                        key={restaurant.id}
+                        position={{
+                            lat: restaurant.latitude,
+                            lng: restaurant.longitude,
+                        }}
+                        title={restaurant.name}
+                        onClick={() => handleMarkerClick(restaurant)}
+                    />
+                ))}
             </GoogleMap>
         </div>
     );
--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -1,51 +1,28 @@
-import React, { useState, useEffect } from "react";
-import { getAllRestaurants } from "../config/firebase";
-import { getDocs, collection } from "firebase/firestore";
-import MarkerDetails from "./MarkerDetails";
+import React from "react";
 
 const InitList = ({
+    restaurantList,
     setRestaurantSelected,
 }: {
+    restaurantList: any[];
     setRestaurantSelected: Function;
 }) => {
-    const [restaurantList, setRestaurantList] = useState<any>([]);
-
-    const [isListItemClicked, setIsListItemClicked] = useState(false);
-
     const handleListItemClick = (restaurant: any) => {
         console.log(restaurant);
         setRestaurantSelected(restaurant);
     };
 
-    // use getRestaurantList method when the List first renders
-    useEffect(() => {
-        const fetchData = async () => {
-            const restaurants = await getAllRestaurants();
-            setRestaurantList(restaurants);
-        };
-
-        fetchData();
-    }, []);
-
     return (
         <div>
             <ul id="restaurantList">
-                {restaurantList.map(
-                    (restaurant: {
-                        id: React.Key | null | undefined;
-                        latitude: any;
-                        longitude: any;
-                        name: string | undefined;
-                        website: string | undefined;
-                    }) => (
-                        <li
-                            key={restaurant.id}
-                            onClick={() => handleListItemClick(restaurant)}
-                        >
-                            {restaurant.name}
-                        </li>
-                    )
-                )}
+                {restaurantList.map((restaurant) => (
+                    <li
+                        key={restaurant.id}
+                        onClick={() => handleListItemClick(restaurant)}
+                    >
+                        {restaurant.name}
+                    </li>
+                ))}
             </ul>
         </div>
     );
