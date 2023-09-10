063324bad01820f988d825992a176005d548fdf0
coriandar <88608040+coriandar@users.noreply.github.com>
2023-09-11 02:24:48 +1200

    Refactor login

--- src/components/account/Login.tsx
+++ src/components/account/Login.tsx
@@ -5,8 +5,6 @@ import { uiConfigSignIn } from "@/config/FirebaseAuthUI.config";
 import StyledFirebaseAuth from "../firebase/StyledFirebaseAuth";
 import { auth } from "../firebase/FirebaseApp";
 import Image from "next/image";
-// import firebase from "firebase/compat/app";
-// import "firebase/compat/auth";
 import Loading from "../loading/Loading";
 
 export default function Login() {
@@ -14,9 +12,9 @@ export default function Login() {
     const router = useRouter();
     const styleConfig = uiConfigSignIn();
 
-    if (user) {
-        return <div>True</div>;
-    } else if (!user) {
+    if (loading) return <Loading />;
+    else if (user) router.replace("/"); // if user is already logged in
+    else if (!user) {
         return (
             <div className="flex flex-col items-center h-full">
                 <Image
@@ -34,35 +32,4 @@ export default function Login() {
             </div>
         );
     }
-
-    // const [loadtime, setLoading] = useState(true);
-
-    // useEffect(() => {
-    //     const timer = setTimeout(() => {
-    //         setLoading(false);
-    //     }, 2000);
-
-    //     return () => clearTimeout(timer); // Cleanup the timer if the component unmounts
-    // }, []);
-
-    // if (loading) return <Loading />;
-    // else if (user) router.replace("/"); // if user is already logged in
-    // if (!user) {
-    //     return (
-    //         <div className="flex flex-col items-center h-full">
-    //             <Image
-    //                 className="m-8"
-    //                 src="/logoCut.png"
-    //                 alt="App Logo"
-    //                 width={200}
-    //                 height={200}
-    //                 priority
-    //             />
-    //             <StyledFirebaseAuth
-    //                 uiConfig={styleConfig}
-    //                 firebaseAuth={auth}
-    //             />
-    //         </div>
-    //     );
-    // } else if (user) router.replace("/"); // if user is already logged in
 }
