78db36ca3be08ab653b56242d9c766c9f0ee200c
coriandar <88608040+coriandar@users.noreply.github.com>
2023-09-14 20:55:42 +1200

    refactor: update background colour

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,5 +2,5 @@ import Loading from "@/components/loading/Loading";
 import React from "react";
 
 export default function aboutPage() {
-    return <div className="bg-slate-50 p-4 h-full">About Page</div>;
+    return <div className="bg-slate-100 p-4 h-full">About Page</div>;
 }
