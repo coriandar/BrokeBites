6803ad03fddb5cb053985a207c95c5ff840e6806
coriandar <88608040+coriandar@users.noreply.github.com>
2023-09-16 04:45:50 +1200

    refactor[nav]: decouple components

---
+++ src/components/navigation/Nav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { auth } from "../firebase/FirebaseApp";
+import { useAuthState } from "react-firebase-hooks/auth";
+import LoggedInBtnSet from "./LoggedInBtnSet";
+import LoggedOutBtnSet from "./LoggedOutBtnSet";
+import NavImage from "./NavImage";
+
+export default function Nav() {
+    const [user, loading] = useAuthState(auth);
+
+    if (loading) {
+        return <div className="bg-slate-300"></div>;
+    }
+
+    return (
+        <nav className="bg-slate-300 flex justify-between w-full h-full p-2 border-b-2">
+            <NavImage />
+            {user ? <LoggedInBtnSet /> : <LoggedOutBtnSet />}
+        </nav>
+    );
+}
