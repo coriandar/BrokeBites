b512baf4764e9db34c54091f89c45ac46dc54efb
coriandar <88608040+coriandar@users.noreply.github.com>
2023-09-16 13:56:15 +1200

    fix[BookmarkFeature]: map loading bug

--- src/components/DashboardSave.jsx
+++ src/components/DashboardSave.jsx
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from "react";
 import {
     getAllRestaurants,
     getFilteredRestaurants,
+    getFilteredPriceRating,
 } from "./firebase/FirebaseApp";
 import InitMap from "./map/Map";
 import InitList from "./restaurant/RestaurantList";
 import MarkerDetails from "./map/MarkerDetails";
+import InitPriceSlider from "./restaurant/PriceSlider";
 import { fetchSavedBitesList } from "./savedBites/SavedBitesList";
 
-export default function DashboardSave() {
+export default function Dashboard() {
     const [restaurantMasterList, setRestaurantMasterList] = useState([]);
     const [restaurantList, setRestaurantList] = useState([]);
     const [restaurantSelected, setRestaurantSelected] = useState(null);
@@ -17,6 +19,7 @@ export default function DashboardSave() {
         lat: -36.8537761039407,
         lng: 174.7658246985396,
     });
+    const [mapZoom, setMapZoom] = useState(17);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -31,14 +34,14 @@ export default function DashboardSave() {
     return (
         <div className="flex h-full">
             <div className="bg-slate-100 m-4 flex justify-center overflow-y-auto no-scrollbar w-1/3">
-                <h3 className="font-bold p-4">Saved Bites</h3>
                 <InitList
                     restaurantList={restaurantList}
                     setRestaurantSelected={setRestaurantSelected}
                     setCenter={setCenter}
                     setRestaurantList={setRestaurantList}
                     getFilteredRestaurants={getFilteredRestaurants}
-                    restaurantMasterList={restaurantMasterList}
+                    restaurantMasterList={restaurantList}
+                    setMapZoom={setMapZoom}
                 />
             </div>
             <div className="bg-slate-300 w-2/3 relative">
@@ -47,11 +50,19 @@ export default function DashboardSave() {
                     setRestaurantSelected={setRestaurantSelected}
                     setCenter={setCenter}
                     center={center}
+                    mapZoom={mapZoom}
+                    setMapZoom={setMapZoom}
+                    restaurantSelected={restaurantSelected}
                 />
                 <div className="bg-slate-300 w-2/3 bg-opacity-90 absolute bottom-0 left-0">
                     <MarkerDetails selected={restaurantSelected} />
                 </div>
             </div>
+            <InitPriceSlider
+                restaurantMasterList={restaurantMasterList}
+                setRestaurantList={setRestaurantList}
+                getFilteredPriceRating={getFilteredPriceRating}
+            />
         </div>
     );
 }
