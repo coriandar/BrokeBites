380f26e18fca177564efea640c25ddbc1c7aac00
Jamie Corstorphine <jvcorstorphine@gmail.com>
2023-09-20 00:49:50 +1200

    feat: Created layout for UserProfile reviews

--- src/components/userProfile/UserProfile.jsx
+++ src/components/userProfile/UserProfile.jsx
@@ -1,15 +1,17 @@
+// UserProfile.js
 import { useState, useEffect } from "react";
 import { db } from "../firebase/FirebaseApp";
-import { collection, getDocs, doc, getDoc } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
+import { fetchSavedBitesList } from "../savedBites/SavedBitesList";
 
-export default function UserProfile({ user }) {
+export default function UserProfile({ uid }) {
     const [userProfile, setUserProfile] = useState(null);
     const [favorites, setFavorites] = useState([]);
 
     useEffect(() => {
         const fetchUserProfile = async () => {
             try {
-                const userDocRef = doc(db, "userDB", user);
+                const userDocRef = doc(db, "userDB", uid); // Use uid to fetch the user document
                 const userDocSnapshot = await getDoc(userDocRef);
                 if (userDocSnapshot.exists()) {
                     const userData = userDocSnapshot.data();
@@ -22,47 +24,55 @@ export default function UserProfile({ user }) {
 
         const fetchFavorites = async () => {
             try {
-                const favoritesRef = collection(
-                    db,
-                    "userDB",
-                    user,
-                    "favorites"
+                // Fetch favorites based on uid
+                const favoriteList = await fetchSavedBitesList(
+                    uid,
+                    "favourite"
                 );
-                const favoritesSnapshot = await getDocs(favoritesRef);
-
-                const userFavorites = favoritesSnapshot.docs.map((doc) => ({
-                    id: doc.id,
-                    ...doc.data(),
-                }));
-
-                setFavorites(userFavorites);
+                setFavorites(favoriteList);
             } catch (error) {
-                console.error("Error fetching favorites:", error);
+                console.error("Error fetching user's favorite list:", error);
             }
         };
 
-        if (user) {
+        const fetchReviews = async () => {};
+
+        // Call fetchUserProfile() and fetchFavorites() only if uid is defined
+        if (uid) {
             fetchUserProfile();
             fetchFavorites();
         }
-    }, [user]);
+    }, [uid]);
 
     return (
         <div>
-            {console.log("userProfile: " + userProfile)}
-            {userProfile ? (
-                <div>
-                    <h1>{userProfile.displayName}'s Profile</h1>
-                    <h2>Favorite List</h2>
-                    <ul>
-                        {favorites.map((favorite) => (
-                            <li key={favorite.id}>{favorite.name}</li>
-                        ))}
-                    </ul>
-                </div>
-            ) : (
-                <p>Loading user profile...</p>
-            )}
+            <h1>{userProfile.displayName}'s Profile</h1> <br />
+            <div id="User reviews">
+                {userProfile ? (
+                    <div>
+                        <h2>Reviews</h2>
+                        <ul>placeholder</ul>
+                    </div>
+                ) : (
+                    <p>Loading user reviews...</p>
+                )}
+            </div>
+            <div id="favourites list">
+                {console.log("userProfile: " + JSON.stringify(userProfile))}
+                {userProfile ? (
+                    <div>
+                        <h1>{userProfile.displayName}'s Profile</h1> <br />
+                        <h2>Favorite List</h2>
+                        <ul>
+                            {favorites.map((favorite) => (
+                                <li key={favorite.id}>{favorite.name}</li>
+                            ))}
+                        </ul>
+                    </div>
+                ) : (
+                    <p>Loading user profile...</p>
+                )}
+            </div>
         </div>
     );
 }
