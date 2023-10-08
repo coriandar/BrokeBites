a69ef4199d969b40809866e679c91136adc91d02
coriandar <88608040+coriandar@users.noreply.github.com>
2023-10-08 18:26:20 +1300

    fix: handle null restaurant

--- src/components/restaurant/printSymbol.js
+++ src/components/restaurant/printSymbol.js
@@ -17,14 +17,14 @@ const printStar = (starRating) => {
 
 export const showSymbol = ({ restaurant, activeFilter }) => {
     if (activeFilter.includes("filling")) {
-        return restaurant.fillingFactor;
+        return restaurant?.fillingFactor;
     } else if (activeFilter.includes("star")) {
-        return printStar(restaurant.starRating);
+        return printStar(restaurant?.starRating);
     } else if (activeFilter.includes("cuisine")) {
         return restaurant?.cuisine;
     } else if (activeFilter.includes("dietary")) {
         return restaurant?.dietary;
     } else {
-        return printPrice(restaurant.priceRating);
+        return printPrice(restaurant?.priceRating);
     }
 };
