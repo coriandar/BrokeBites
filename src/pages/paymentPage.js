2a299a5739659963fc223e5a003e975c56161063
mnymkz <kmd2088@autuni.ac.nz>
2023-10-19 11:20:03 +1300

    test: testing for updating premium database. This commit adds testing buttons for adding and removing premium from a user in a db.

--- src/pages/paymentPage.js
+++ src/pages/paymentPage.js
@@ -1,26 +1,44 @@
 //page for payments containing the stripe payment container
 import React from "react";
-import { checkout } from "../checkout";
+import { checkout } from "../stripe/checkout";
+//testing premium methods
+// import { setPremium } from "../database/firebase/firestore/userDB";
+// import { removePremium } from "../database/firebase/firestore/userDB";
 
 function paymentPage() {
     //return a payment button
     return (
-        <button
-            className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-            onClick={() => {
-                console.log("clicked");
-                checkout({
-                    lineItems: [
-                        {
-                            price: "price_1O2Pb3H0glciU1R3D3h3AJtD",
-                            quantity: 1,
-                        },
-                    ],
-                });
-            }}
-        >
-            BUY!
-        </button>
+        <>
+            <button
+                className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+                onClick={() => {
+                    console.log("clicked");
+                    checkout({
+                        lineItems: [
+                            {
+                                price: "price_1O2Pb3H0glciU1R3D3h3AJtD",
+                                quantity: 1,
+                            },
+                        ],
+                    });
+                }}
+            >
+                BUY!
+            </button>
+            {/* Testing premium for users */}
+            {/* <button
+                className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+                onClick={setPremium}
+            >
+                setPremium
+            </button>
+            <button
+                className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+                onClick={removePremium}
+            >
+                removePremium
+            </button> */}
+        </>
     );
 }
 
