aad332da8a713cc88ffeda9931bba6d8119b3a2f
mnymkz <kmd2088@autuni.ac.nz>
2023-10-19 11:14:07 +1300

    feat: adds code for the payment success page. This commit adds verification for the paymentSuccess page.

--- src/pages/paymentSuccess.js
+++ src/pages/paymentSuccess.js
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import React from "react";
+import verifyPayment from "../stripe/verifyPayment";
 
 export default function paymentSuccess() {
-    retrun(
+    //hook for payment status
+    const [paymentStatus, setPaymentStatus] = useState(null);
+
+    useEffect(() => {
+        const queryParams = new URLSearchParams(window.location.search);
+        const sessionId = queryParams.get("session_id");
+
+        if (sessionId) {
+            verifyPayment(sessionId).then((data) => {
+                setPaymentStatus(data);
+            });
+        }
+    }, []);
+
+    return (
         <div>
-            <h1>Payment Success</h1>
-        </div>,
+            {paymentStatus === "success" && <p>Payment was successful!</p>}
+            {paymentStatus === "failed" && <p>Payment was not successful.</p>}
+            {paymentStatus === "error" && (
+                <p>There was an error verifying the payment status.</p>
+            )}
+        </div>
     );
 }
