87c9f3c35237e3d21702dfedb7635756c6756466
Jamie Lee <jamielee@Jamies-MacBook-Pro-3.local>
2023-10-21 22:26:12 +1300

    feat[DirectMessage]: messages get displayed

--- src/components/directMessage/component/Messages.jsx
+++ src/components/directMessage/component/Messages.jsx
@@ -1,17 +1,59 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { SelectedChat } from "../logic/SelectedChatContext";
 import { getMessages } from "@/database/firebase/firestore/direcMessageDB";
+import { auth } from "@/database/firebase/firebaseApp";
 
 export default function Messages() {
     const { data } = useContext(SelectedChat);
     const [messages, setMessages] = useState([]);
+    const currentUser = auth.currentUser.displayName;
 
-    console.log("SelectedChat context data: ", data?.selectedChat?.id);
+    console.log("CurrentUser in Messages: ", currentUser);
 
-    const displayMessages = () => {
-        setMessages(getMessages(data.selectedChat.id));
-        console.log("Messages in dispayMessages: ", messages);
+    useEffect(() => {
+        displayMessages();
+    }, [data]);
+
+    const displayMessages = async () => {
+        if (data && data.selectedChat) {
+            const messages = await getMessages(data.selectedChat.id);
+            setMessages(messages);
+        }
     };
 
-    return <div className="messageContainer">Messages go here</div>;
+    const formatTimestamp = (timestamp) => {
+        const date = new Date(timestamp.seconds * 1000);
+        const options = {
+            day: "numeric",
+            month: "numeric",
+            year: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+        };
+        return date.toLocaleString("en-NZ", options);
+    };
+
+    return (
+        <div className="messageContainer">
+            <ul className="messageList">
+                {messages.map((message) => (
+                    <li key={message.id} className="messageItem">
+                        {console.log(message.sender)}
+                        {message.sender === currentUser ? (
+                            <div className="sentMessage">
+                                {message.messageText}
+                            </div>
+                        ) : (
+                            <div className="recievedMessage">
+                                {message.messageText}
+                            </div>
+                        )}
+                        <div className="timestamp">
+                            {formatTimestamp(message.timestamp)}
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
