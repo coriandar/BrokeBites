b06d3cdd33dbaa13965aeec62b277abb0a86bc46
coriandar <88608040+coriandar@users.noreply.github.com>
2023-10-22 05:43:07 +1300

    feat[recommendation]: complete, add spinner, disable while loading

--- src/components/recommendation/components/RecommendContainer.jsx
+++ src/components/recommendation/components/RecommendContainer.jsx
@@ -6,14 +6,14 @@ export default function RecommendContainer({ restaurants }) {
         <div className="m-8 flex h-full w-full flex-col items-center justify-center rounded-lg">
             <h3 className="text-lg font-bold">Recommendations</h3>
             <ul>
-                {restaurants ? (
+                {restaurants.length > 0 ? (
                     restaurants.map((restaurant) => (
                         <li key={`${restaurant.id}`}>
                             <RecommendCard restaurant={restaurant} />
                         </li>
                     ))
                 ) : (
-                    <p>loading...</p>
+                    <p>Loading...</p>
                 )}
             </ul>
         </div>
