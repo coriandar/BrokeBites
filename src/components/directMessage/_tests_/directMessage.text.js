43139ff909f2c10011580972078168b08353cf99
lleejamiee <85136381+lleejamiee@users.noreply.github.com>
2023-10-22 10:35:04 +1300

    Test commit to check the author

---
+++ src/components/directMessage/_tests_/directMessage.text.js
@@ -0,0 +1,8 @@
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+describe("DirectMessage", () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+});
