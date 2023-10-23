184dbc7122a63a5a37b75d04ff564660724401b4
coriandar <88608040+coriandar@users.noreply.github.com>
2023-10-24 02:03:14 +1300

    fix[reviewModal]: render too many hooks

---
+++ src/components/review/ReviewCardModal.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReviewCard from "./ReviewCard";
+import Link from "next/link";
+import { formatTimestamp } from "../../util/dateFunctions";
+import Avatar from "../account/Avatar";
+import FlagButton from "../moderation/FlagButton";
+
+export default function ReviewCardModal({ review }) {
+    return (
+        <ReviewCard>
+            <Link href={`/profile/${review.userID}`}>
+                <Avatar maxW={"w-[75px]"} photoURL={review.photoURL} />
+            </Link>
+            <div className="ml-4 w-full">
+                <div className="flex h-8 w-full items-center justify-between">
+                    <Link href={`/profile/${review.userID}`}>
+                        {review.userName || "Anonymous"}
+                    </Link>
+                    <p className="text-xs font-light">
+                        {formatTimestamp(review.timestamp)}
+                    </p>
+                </div>
+                <p>{review.reviewText}</p>
+            </div>
+            <FlagButton review={review} />
+        </ReviewCard>
+    );
+}
