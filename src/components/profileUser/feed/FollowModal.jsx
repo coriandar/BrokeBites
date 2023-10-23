4c9d5bcb5482db8899a9826a3a55b1fb94833533
Jamie Corstorphine <jvcorstorphine@gmail.com>
2023-10-23 14:00:48 +1300

    feat: Added checking for feed post type and started work on favourites type

--- src/components/profileUser/feed/FollowModal.jsx
+++ src/components/profileUser/feed/FollowModal.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchUser } from "@/database/firebase/firestore/userDB";
 
 export default function FollowContainer({ followData, displayName }) {
+    const [recipientDisplayName, setRecipientDisplayName] = useState(null);
+
+    useEffect(() => {
+        const fetchRecipientDisplayName = async () => {
+            try {
+                const user = await fetchUser(followData.recipient);
+                if (user) {
+                    setRecipientDisplayName(user.displayName);
+                } else {
+                    setRecipientDisplayName("Display Name Not Found");
+                }
+            } catch (error) {
+                console.error(
+                    "Error fetching recipient's display name:",
+                    error,
+                );
+                setRecipientDisplayName("Display Name Error");
+            }
+        };
+
+        fetchRecipientDisplayName();
+    }, [followData.recipient]);
+
     return (
         <div>
             <ul key={followData.id} className="w-full">
-                {displayName} followed {followData.recipient}
+                {displayName} followed {recipientDisplayName}
             </ul>
         </div>
     );
--- src/database/firebase/firestore/userFeedDB.js
+++ src/database/firebase/firestore/userFeedDB.js
@@ -27,3 +27,29 @@ export const fetchFeed = async (userID) => {
         return null;
     }
 };
+
+export const fetchDisplayName = async (userID) => {
+    // Reference the userDB collection
+    const userCollectionRef = collection(db, "userDB");
+
+    // Create a query to filter documents by userID
+    const q = query(userCollectionRef, where("uid", "==", userID));
+
+    try {
+        // Fetch the documents that match the query
+        const userQuerySnapshot = await getDocs(q);
+
+        if (!userQuerySnapshot.empty) {
+            // Assuming there is only one document with the given userID
+            const userDoc = userQuerySnapshot.docs[0];
+            const userData = userDoc.data();
+            return userData.displayName; // Retrieve the displayName from the user document
+        } else {
+            console.log("User not found or does not exist.");
+            return null;
+        }
+    } catch (error) {
+        console.error("Error fetching user data:", error);
+        return null;
+    }
+};
