ac8bc395bde55a973ce29e243e12eb376d533d7f
coriandar <88608040+coriandar@users.noreply.github.com>
2023-10-24 04:42:29 +1300

    fix[errors]: usestateErrors

--- src/pages/paymentPage.js
+++ src/pages/paymentPage.js
@@ -1,46 +1,26 @@
 //page for payments containing the stripe payment container
 import React from "react";
 import { checkout } from "../stripe/checkout";
-//testing premium methods
-// import {
-//     checkPremiumStatus,
-//     setPremium,
-//     removePremium,
-// } from "../database/firebase/firestore/userDB";
 
 function paymentPage() {
     //return a payment button
     return (
-        <>
-            <button
-                className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-                onClick={() => {
-                    console.log("clicked");
-                    checkout({
-                        lineItems: [
-                            {
-                                price: "price_1O2Pb3H0glciU1R3D3h3AJtD",
-                                quantity: 1,
-                            },
-                        ],
-                    });
-                }}
-            >
-                BUY!
-            </button>
-            {/* <button
-                className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-                onClick={setPremium}
-            >
-                setPremium
-            </button>
-            <button
-                className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-                onClick={removePremium}
-            >
-                removePremium
-            </button> */}
-        </>
+        <button
+            className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+            onClick={() => {
+                console.log("clicked");
+                checkout({
+                    lineItems: [
+                        {
+                            price: "price_1O2Pb3H0glciU1R3D3h3AJtD",
+                            quantity: 1,
+                        },
+                    ],
+                });
+            }}
+        >
+            BUY!
+        </button>
     );
 }
 
--- src/pages/unsubscribeSuccess.js
+++ src/pages/unsubscribeSuccess.js
@@ -1,29 +1,34 @@
-//successful unsubscribe page
-import { useState, useEffect } from "react";
-import { checkPremiumStatus } from "@/database/firebase/firestore/userDB";
-import Link from "next/link";
+// //successful unsubscribe page
+// import { useState, useEffect } from "react";
+// import { checkPremiumStatus } from "@/database/firebase/firestore/userDB";
+// import Link from "next/link";
 
+// export default function unsubSuccess() {
+//     const [isPremium, setIsPremium] = useState(false); //hook for premium status
+//     useEffect(() => {
+//         checkPremiumStatus().then((premium) => {
+//             setIsPremium(premium);
+//         });
+//     }, []);
+
+//     //double check if user is unsubscribed
+//     return (
+//         <div>
+//             {isPremium ? (
+//                 <h1>Error unsubscribing</h1>
+//             ) : (
+//                 <h1>Successfuly unsubscribed</h1>
+//             )}
+//             <Link href="/profile">
+//                 <button className="m-4 w-40 rounded-md bg-slate-200 px-4 py-1">
+//                     Return
+//                 </button>
+//             </Link>
+//         </div>
+//     );
+// }
+
+import UnsubSuccess from "@/components/premium/UnsubscribeSuccess";
 export default function unsubSuccess() {
-    const [isPremium, setIsPremium] = useState(false); //hook for premium status
-    useEffect(() => {
-        checkPremiumStatus().then((premium) => {
-            setIsPremium(premium);
-        });
-    }, []);
-
-    //double check if user is unsubscribed
-    return (
-        <div>
-            {isPremium ? (
-                <h1>Error unsubscribing</h1>
-            ) : (
-                <h1>Successfuly unsubscribed</h1>
-            )}
-            <Link href="/profile">
-                <button className="m-4 w-40 rounded-md bg-slate-200 px-4 py-1">
-                    Return
-                </button>
-            </Link>
-        </div>
-    );
+    return <UnsubSuccess />;
 }
