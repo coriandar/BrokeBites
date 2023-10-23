f31808cddf521cfa2771f941d5c4206c9b768161
lleejamiee <85136381+lleejamiee@users.noreply.github.com>
2023-10-23 23:34:42 +1300

    test[DirectMessage]: add tests

--- src/components/directMessage/_tests_/directMessage.text.js
+++
@@ -1,8 +0,0 @@
-import { render, fireEvent } from "@testing-library/react";
-import "@testing-library/jest-dom";
-
-describe("DirectMessage", () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
-});
