f180c421382c8a24bc0cacacf1199081c40cfd53
coriandar <88608040+coriandar@users.noreply.github.com>
2023-10-23 16:36:49 +1300

    feat[iconButton]: add icons for sortBy

--- src/components/sort/components/SortBy.jsx
+++ src/components/sort/components/SortBy.jsx
@@ -1,4 +1,10 @@
 import React, { useState } from "react";
+import {
+    ArrowUpDown,
+    ArrowDownWideNarrow,
+    ArrowUpNarrowWide,
+} from "lucide-react";
+import { BottomTooltip } from "@/components/ui/tooltip/Tooltip";
 
 export default function SortBy({
     restaurantMasterList,
@@ -30,28 +36,31 @@ export default function SortBy({
     return (
         <div className="flex h-14 w-11/12 items-center justify-center rounded-lg bg-slate-300 pl-2 shadow-lg">
             <button
+                title="Default"
                 className={`m-1 rounded-md px-4 py-1 text-xs ${changeColour(
                     "default",
                 )}`}
                 onClick={() => handleClick("default")}
             >
-                Default
+                <ArrowUpDown className="h-[1.2rem] w-[1.2rem]" />
             </button>
             <button
+                title="Acsending"
                 className={`m-1 rounded-md px-4 py-1 text-xs ${changeColour(
                     "ascending",
                 )}`}
                 onClick={() => handleClick("ascending")}
             >
-                Ascending
+                <ArrowUpNarrowWide className="h-[1.2rem] w-[1.2rem]" />
             </button>
             <button
+                title="Descending"
                 className={`m-1 rounded-md px-4 py-1 text-xs ${changeColour(
                     "descending",
                 )}`}
                 onClick={() => handleClick("descending")}
             >
-                Descending
+                <ArrowDownWideNarrow className="h-[1.2rem] w-[1.2rem]" />
             </button>
         </div>
     );
