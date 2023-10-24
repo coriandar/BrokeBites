88346b7c9310a0a59d7f9376c042b63e4ff6773a
mnymkz <kmd2088@autuni.ac.nz>
2023-10-24 20:36:04 +1300

    feat: adds now take user to the payment page. This commit adds a link to the add component that takes users to the payment page.

--- src/components/ads/BannerAd.js
+++ src/components/ads/BannerAd.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Link from "next/link";
 
 const BannerAd = ({ isPremium }) => {
     const [isBannerVisible, setIsBannerVisible] = useState(true);
@@ -10,15 +11,17 @@ const BannerAd = ({ isPremium }) => {
     return isPremium
         ? null
         : isBannerVisible && (
-              <div className="fixed right-8 top-20 z-50 flex h-[100px] w-[1000px] items-center justify-between rounded-2xl bg-background p-4 text-accent-foreground">
-                  <span className="text-xl text-gray-600">Ad Banner</span>
-                  <button
-                      onClick={hideBanner}
-                      className="cursor-pointer text-xl text-gray-600"
-                  >
-                      X
-                  </button>
-              </div>
+              <Link href={"/paymentPage"}>
+                  <div className="fixed right-8 top-20 z-50 flex h-[100px] w-[1000px] items-center justify-between rounded-2xl bg-background p-4 text-accent-foreground">
+                      <span className="text-xl text-gray-600">Ad Banner</span>
+                      <button
+                          onClick={hideBanner}
+                          className="cursor-pointer rounded-md border border-gray-600 p-2 text-xl text-gray-600"
+                      >
+                          X
+                      </button>
+                  </div>
+              </Link>
           );
 };
 
